1b53f20b7c3bc4b81925da501595d8e469bc12c3
juliovianadev <juliovianadev@gmail.com>
2023-06-14 18:11:53 -0300

    first interface

--- src/app.ts
+++ src/app.ts
@@ -1,71 +1,16 @@
-class Department {
-    // private id: string;
-    // private name: string;
-    protected employees: string[] = [];
-  
-    constructor(private readonly id: string, public name: string) {
-      // this.id = id;
-      // this.name = n;
-    }
-  
-    describe(this: Department) {
-      console.log(`Department (${this.id}): ${this.name}`);
-    }
-  
-    addEmployee(employee: string) {
-      // validation etc
-      this.employees.push(employee);
-    }
-  
-    printEmployeeInformation() {
-      console.log(this.employees.length);
-      console.log(this.employees);
-    }
-  }
-  
-  class ItDepartament extends Department{
-    admins: string[];
-    constructor(id:string, admins: string[]){
-      super(id, 'IT') 
-      this.admins = admins
-    }
-  }
-  class AccountingDepartment extends Department{
-    private lastReport:string;
+interface Person{
+ name: string;
+ age: number;
+ greet(phrase: string): void 
+}
 
-    constructor(id: string,private reports: string[]){
-      super(id,"Account")
-      this.lastReport = reports[0];
-    }
-    addEmployeeName(name: string) {
-      if (name ==='Tomate'){
-        return
-      }
-      this.employees.push(name)
-    }
-    addReport(text:string){
-      this.reports.push(text)
-    }
-    getReports(){
-      console.log(this.reports)
-    }
+let user1: Person;
+user1 = {
+  name: 'tomate',
+  age: 12,
+  greet(phrase: string){
+    console.log(phrase + ' ' + this.name)
   }
+}
 
-   const it = new ItDepartament('d1', ['tomate']);
-  
-  it.addEmployee('Tomate');
-  it.addEmployee('Manu');
-  
-  // accounting.employees[2] = 'Anna';
-  
-  it.describe();
-  it.name = 'Tomatao NAME';
-  it.printEmployeeInformation();
-  console.log(it)
-  // const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
-  
-  // accountingCopy.describe();
-  const account = new AccountingDepartment('d2',[]);
-  account.addReport('reportado')
-  account.getReports()
-  account.addEmployeeName('ola')
+user1.greet('tomate é bao')
---
+++ src/classes.ts
@@ -0,0 +1,86 @@
+class Department {
+    // private id: string;
+    // private name: string;
+    protected employees: string[] = [];
+  
+    constructor(private readonly id: string, public name: string) {
+      // this.id = id;
+      // this.name = n;
+    }
+  
+    describe(this: Department) {
+      console.log(`Department (${this.id}): ${this.name}`);
+    }
+  
+    addEmployee(employee: string) {
+      // validation etc
+      this.employees.push(employee);
+    }
+  
+    printEmployeeInformation() {
+      console.log(this.employees.length);
+      console.log(this.employees);
+    }
+  }
+  
+  class ItDepartament extends Department{
+    admins: string[];
+    constructor(id:string, admins: string[]){
+      super(id, 'IT') 
+      this.admins = admins
+    }
+  }
+  class AccountingDepartment extends Department{
+    private lastReport: string;
+
+    get mostRecentReport() {
+      if (this.lastReport) {
+        return this.lastReport;
+      }
+      throw new Error('No report found.');
+    }
+    set mostRecentReport(value: string) {
+      if (!value) {
+        throw new Error('Please pass in a valid value!');
+      }
+      this.addReport(value);
+    }
+
+    constructor(id: string,private reports: string[]){
+      super(id,"Account")
+      this.lastReport = reports[0];
+    }
+    addEmployeeName(name: string) {
+      if (name ==='Tomate'){
+        return
+      }
+      this.employees.push(name)
+    }
+    addReport(text:string){
+      this.reports.push(text)
+    }
+    getReports(){
+      console.log(this.reports)
+    }
+  }
+
+   const it = new ItDepartament('d1', ['tomate']);
+  
+  it.addEmployee('Tomate');
+  it.addEmployee('Manu');
+  
+  // accounting.employees[2] = 'Anna';
+  
+  it.describe();
+  it.name = 'Tomatao NAME';
+  it.printEmployeeInformation();
+  console.log(it)
+  // const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+  
+  // accountingCopy.describe();
+  const account = new AccountingDepartment('d2',[]);
+  account.addReport('reportado')
+  account.getReports()
+  account.addEmployeeName('ola')
+
+  console.log(account.mostRecentReport);
